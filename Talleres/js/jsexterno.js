c6f2f04131fad9403b499287e54e0141c90f9b34
Chriztian2000 <cabetancourt@uce.edu.ec>
2024-11-15 14:37:26 -0500

    tarea4.1

--- Talleres/js/jsexterno.js
+++ Talleres/js/jsexterno.js
@@ -1,3 +1,11 @@
-function nombreFuncion(id, cambio){
+function cambiarFuente(id, cambio){
     document.getElementById(id).style.fontFamily=cambio;
 }
+
+function cambiarTamano(id, cambio){
+    document.getElementById(id).style.fontSize=cambio;
+}
+
+function cambiarBackground(id, cambio){
+    document.getElementById(id).style.background=cambio;
+}    
