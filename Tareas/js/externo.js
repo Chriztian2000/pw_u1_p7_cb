fbb706aac90a2b88b59a3c1d7070e7ceea2d82ed
Chriztian2000 <cabetancourt@uce.edu.ec>
2024-11-29 23:30:44 +0000

    taller12.2

---
+++ Tareas/js/externo.js
@@ -0,0 +1,31 @@
+function cambiarBackground(id, cambio){
+    document.getElementById(id).style.background=cambio;
+}
+
+function cambiarColorTexto(id, cambio){
+    document.getElementById(id).style.color=cambio;
+}
+
+function cambiarEstiloLetra(id, cambio){
+    document.getElementById(id).style.fontStyle=cambio;
+}
+
+function cambiarPesoLetra(id, cambio){
+    document.getElementById(id).style.fontWeight=cambio;
+}
+
+function cambiarTamanioTexto(id, cambio){
+    document.getElementById(id).style.fontSize=cambio;
+}
+
+function cambiarTipoLetra(id, cambio){
+    document.getElementById(id).style.fontFamily=cambio;
+}
+
+function cambiarTexto(id, cambio){
+    document.getElementById(id).innerText=cambio;
+}
+
+function alinearTexto(id, cambio){
+    document.getElementById(id).style.textAlign=cambio;
+}
